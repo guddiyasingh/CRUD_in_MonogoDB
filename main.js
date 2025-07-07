0d97cf8dd6ea02fc132224f81766485af2155cd9
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-07-07 16:07:41 +0530

    add code in main file

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 import mongoose from "mongoose";
 
 let a = await mongoose.connect("mongodb://localhost:27017/")
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
